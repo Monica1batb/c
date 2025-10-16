b81f92e29c58318c796385b42ad4372661d8f7ae
Monica1batb <monica171274@hotmail.com>
2025-10-16 09:57:53 +0200

    Create mitjana2.c

---
+++ mitjana2.c
@@ -0,0 +1,15 @@
+ /*********************************
+  * Programa que calcula la mitjana
+  * artmètica de 2 enters
+  * Mónica PXSI 25-26 ************/
+ #include <stdio.h>
+ void main (){
+     int a, b;
+     float mitjana;
+     printf("1r núm?: ");
+     scanf("%d", &a);
+     printf("2n núm?: ");
+     scanf("%d", &b);
+     printf("La mitjana es: %f\n", (a+b)/2.0);
+     return;
+ }
