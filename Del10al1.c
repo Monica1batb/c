7c82709767fead6c896c1870579cfbc3ff96d14a
Monica1batb <monica171274@hotmail.com>
2025-11-14 12:21:11 +0100

    Add files via upload

---
+++ Del10al1.c
@@ -0,0 +1,12 @@
+/*************************************
+ * Escriu del 10 al 1 per terminal   *
+ * Mónica Moreno - 1bat - PSIX - 25-26 *
+ * **********************************/
+# include <stdio.h>
+void main(){
+    int i;
+    for(i=10;i>=1;i--)
+        printf("%d, ",i);
+    printf("\n");
+    return;
+}
---
+++ bidimensional.c
@@ -0,0 +1,15 @@
+ /*************************************
+  * Escriu del 10 al 1 per terminal   *
+  * Mónica Moreno - 1bat - PSIX -25-26*
+  * **********************************/
+  # include <stdio.h>
+  void main(){
+     int i, j;
+     for(i=1;i<=10;i++)
+         {
+         for(j=1;j<=10;j++)
+             printf("%d, ",i);
+     printf("\n");
+      }
+  return;
+  }
